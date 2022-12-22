7da82287b5afb9d83208122b72b2f47916632685
Geoffrey Gao <geoffreygao1@gmail.com>
2022-12-22 14:06:13 +0900

    Added basic event handler for constructing age

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@ import './css/styles.css';
 
 import Age from "./age.js";
 
-let testAge = new Age();
+document.querySelector("#birthdayInput").addEventListener("submit", convertAgeHandler);
+
+function convertAgeHandler(event) {
+  event.preventDefault();
+  const date = new Date();
+  let age = new Age();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  age.todaysDate = `${year}-${month}-${day}`;
+  age.birthday = document.getElementById('birthday').value;
+  age.findDaysOld();
+  populateAges(age);
+}
